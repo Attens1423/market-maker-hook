5f75e8158c10d6f57886e12076653aea8fa7018d
Ashu Chan <ashu.sakamoto@gmail.com>
2024-10-19 22:18:19 -0700

    chore: add github button

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -1,17 +1,37 @@
 import type { NextPage } from "next";
+import GitHubButton from "react-github-btn";
 import { TimelineDemo } from "~~/components/timeline";
 import { TextHoverEffect } from "~~/components/ui/text-hover-effect";
 
 const Home: NextPage = () => {
   return (
     <div className="max-w-4xl mx-auto">
-      <div className="flex flex-col items-center justify-center my-40">
+      <div className="flex flex-col items-center justify-center mt-36">
+        <GitHubButton
+          href="https://github.com/Attens1423/market-maker-hook"
+          data-color-scheme="no-preference: light; light: light; dark: dark;"
+          data-icon="octicon-star"
+          data-size="large"
+          aria-label="Star Attens1423/market-maker-hook on GitHub"
+        >
+          Star
+        </GitHubButton>
+
         <TextHoverEffect text="UNIHOOK" />
+
         <p className="text-center text-neutral-200 max-w-2xl">
-          Aggregator Hook 旨在充当 “桥梁”，使得市场上的流动性能直接链接到 Uniswap V4 的池子；同时利用 “及时原则”
-          动态管理流动性资金，在交易前注入流动性，在交易后撤出流动性，将流动性迁移对原池子的影响最小化。这种集成使 LPs
-          能夜以前所未有的便捷方式管理资金，利用 Uniswap 的坚固架构，同时利用更广泛的 DEX 生态系统中的流动性。
+          <a href="https://github.com/Attens1423/market-maker-hook" className="text-pink-400">
+            Market Maker Hook
+          </a>{" "}
+          旨在在{" "}
+          <a href="https://github.com/Attens1423/Aggregator-Hook" className="text-pink-400">
+            Aggregator Hook
+          </a>{" "}
+          的基础上拓展一个实用案例：使用一个 MatchEngine 维护做市商们提供的 orderbook，当用户发起交易时，使用 Aggregator
+          Hook 的技术填充流动性，使得用户能直接使用 orderbook 的报价。这种设计也使得做市商无缝接入 Uniswap V4
+          生态成为可能。
         </p>
+        <p className="font-mono text-center text-pink-400 max-w-2xl">Aggregator // Order Limit // Tick Mapping</p>
       </div>
       <TimelineDemo />
     </div>
